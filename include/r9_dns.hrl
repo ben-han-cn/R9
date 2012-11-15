3f63a5bfcd0d2a63201e605cd068e1b4d4b93428
hanfeng <hanfeng@knet.cn>
2012-11-15 23:56:37 +0800

    refine wire name code, split wire name into labels, clear the function definition

--- include/r9_dns.hrl
+++ include/r9_dns.hrl
@@ -52,4 +52,27 @@
                      "UPDATE"]).
 
 
+-define(TYPE_A,1).      % host address
+-define(TYPE_NS,2).     % Authoritive Name Server 
+-define(TYPE_MD,3).     % Mail Destination (Obsolete)
+-define(TYPE_MF,4).     % Mail Forwarder (Obsolete)
+-define(TYPE_CNAME,5).  % Canonical Name for an Alias
+-define(TYPE_SOA,6).    % Start of Authority
+-define(TYPE_MB,7).     % Mailbox Domain Name (Experimental)
+-define(TYPE_MG,8).     % Mail Group Member (Experimental)
+-define(TYPE_MR,9).     % Mail Rename Domain Name (Experimental)
+-define(TYPE_NULL,10).  % NULL RR (Experimental)
+-define(TYPE_WKS,11).   % Well Known Service Description
+-define(TYPE_PTR,12).   % Domain Name Pointer
+-define(TYPE_HINFO,13). % Host Information
+-define(TYPE_MINFO,14). % mailbox or mail list information
+-define(TYPE_MX,15).    % Mail eXchange
+-define(TYPE_TXT,16).   % Text Strings
+
+-define(A_ROOT_SERVERS_NET,{198,41,0,4}).
+
+-define(QTYPE_AXFR,252).     % Request a zone transfer
+-define(QTYPE_MAILB,253).    % Request mailbox-related records (MB, MR or MR)
+-define(QTYPE_MAILA,254).    % Request Mail Agent RR (Obsolete)
+-define(QTYPE_WILDCARD,255). % Request All Records
 -define(TEST, true).
--- src/r9_wire_name.erl
+++ src/r9_wire_name.erl
@@ -1,12 +1,13 @@
 -module(r9_wire_name).
 -export([from_wire/1,
          from_string/1,
+         to_string/1,
+         to_wire/1,
          len/1,
          label_count/1,
          compare/2,
          is_equal/2,
          contains/2,
-         to_string/1,
          substract/2,
          concat/2,
          parent/1]).
@@ -24,18 +25,64 @@
 %% For wire name, it's a little confused that, the ending zero stands for one label,
 %% so for "a.b.com", there are 4 labels, 
 -define(EMPTY_LABEL, #label{len = 1, str = ""}).
+-define(MAX_LABEL_LEN, 63).
+-define(MAX_NAME_LEN, 255).  
 
-labels_from_wire(WireData, Labels) ->
-    <<Len:8/integer, _Left/bits>> = WireData,
-    case Len of 
-        0 -> lists:reverse([?EMPTY_LABEL | Labels]);
-        _ -> {<<_Len:8/integer, LabelBinary/bits>>, <<RestLabels/bits>>} = split_binary(WireData, Len + 1),
-            labels_from_wire(RestLabels, [#label{len = Len + 1, str = string:to_lower(binary_to_list(LabelBinary))} | Labels])
-    end.
 
+%label API
 label_from_string(Str) ->
     #label{len = length(Str) + 1,
-           str = Str}.
+           str = string:to_lower(Str)}.
+
+label_to_string(#label{str = Str}) ->
+    Str.
+
+label_from_wire(<<Len:8/integer, NameStr:Len/bytes, LeftData/bits>>) ->
+    Label = if 
+                Len == 0 -> ?EMPTY_LABEL;
+                (Len < ?MAX_LABEL_LEN)  -> #label{len = Len + 1, 
+                                                  str = string:to_lower(binary_to_list(NameStr))}
+            end,
+    {Label, LeftData}.
+
+label_to_wire(#label{len = Len, str = Str}) ->
+    list_to_binary([Len - 1, Str]).
+
+label_is_equal(#label{len = Len1, str = Str1}, #label{len = Len2, str = Str2}) ->
+    (Len1 == Len2) and (Str1 == Str2).
+
+label_compare(#label{str = Str1}, #label{str = Str2})->
+    if 
+        Str1 > Str2 -> 1;
+        Str1 < Str2 -> -1;
+        true -> 0
+    end.
+
+
+labels_compare([], []) -> 0;
+labels_compare([], _) -> -1;
+labels_compare(_, []) -> 1;
+labels_compare(Labels1, Labels2) ->
+    {[FirstLabel1 | Labels1Left], [FirstLabel2 | Labels2Left] } = {Labels1, Labels2},
+    case label_compare(FirstLabel1, FirstLabel2) of
+        1 -> 1;
+        -1 -> -1;
+        0 -> labels_compare(Labels1Left, Labels2Left)
+    end.
+
+
+%% labels API
+labels_from_wire_helper(WireData, Acc) ->
+    {Label, LeftData}  = label_from_wire(WireData),
+    case Label#label.len of
+        1 -> {[Label | Acc], LeftData};
+        _ -> labels_from_wire_helper(LeftData, [Label | Acc])
+    end.
+
+labels_from_wire(WireData) ->
+    {Labels, LeftData} = labels_from_wire_helper(WireData, []),
+    {lists:reverse(Labels), LeftData}.
+
 
 labels_from_string(Str) ->
     if
@@ -44,34 +91,43 @@ labels_from_string(Str) ->
                              [?EMPTY_LABEL])
     end.
 
-label_to_string(#label{str = Str} = _Label) ->
-    Str.
+labels_is_equal([], []) -> true;
+labels_is_equal(Labels1, Labels2) ->
+    io:format("<<<<< ~p ~p >> ~n", [Labels1, Labels2]),
+    {[Label1 | Labels1Left], [Label2 | Labels2Left]} = {Labels1, Labels2},
+    label_is_equal(Label1, Label2) and labels_is_equal(Labels1Left, Labels2Left).
+    
+labels_to_string(Labels) ->
+    string:join(lists:map(fun(Label) -> label_to_string(Label) end, Labels), ".").
 
+labels_to_wire(Labels) ->
+    list_to_binary(lists:map(fun(Label) -> label_to_wire(Label) end, Labels)).
+
+%%wirename API
 from_lables(Labels) ->
     #wire_name{len = lists:foldl(fun(#label{len = Len} = _Label, Sum) -> Sum + Len end, 0, Labels), 
                label_count = length(Labels),
                labels = Labels}.
 
-from_wire(WireData) when is_bitstring(WireData) ->
-    from_lables(labels_from_wire(WireData, [])).
+from_wire(WireData) ->
+    {Labels, LeftData} = labels_from_wire(WireData),
+    {from_lables(Labels), LeftData}.
+
+to_wire(#wire_name{labels = Labels}) ->
+    labels_to_wire(Labels).
 
 from_string(Str) ->
     from_lables(labels_from_string(Str)).
 
-to_string(#wire_name{labels = Labels} = _Name) ->
-    string:join(lists:map(fun(Label) -> label_to_string(Label) end, Labels), ".").
+to_string(#wire_name{labels = Labels}) ->
+    labels_to_string(Labels).
         
 len(#wire_name{len = Len} = _Name) ->
     Len.
 
-label_count(#wire_name{label_count = Count} = _Name) ->
+label_count(#wire_name{label_count = Count}) ->
     Count.
 
-labels_is_equal([], []) -> true;
-labels_is_equal(Labels1, Labels2) ->
-    {[Label1 | Labels1Left], [Label2 | Labels2Left]} = {Labels1, Labels2},
-    (Label1#label.len == Label2#label.len) and (Label1#label.str == Label2#label.str) and labels_is_equal(Labels1Left, Labels2Left).
-    
 is_equal(Name1, Name2) when is_record(Name1, wire_name) and is_record(Name2, wire_name) ->
     (len(Name1) == len(Name2)) and 
     (label_count(Name1) == label_count(Name2)) and 
@@ -81,48 +137,27 @@ is_equal(_, _) -> false.
 contains(Longer, Shorter) when is_record(Longer, wire_name) and is_record(Shorter, wire_name) ->
     (len(Longer) >= len(Shorter)) and 
     (label_count(Longer) >= label_count(Shorter)) and 
-    labels_is_equal(lists:sublist(Longer#wire_name.labels, label_count(Longer) - label_count(Shorter) + 1, label_count(Shorter)),
+    labels_is_equal(lists:sublist(Longer#wire_name.labels, 
+                                  label_count(Longer) - label_count(Shorter) + 1, 
+                                  label_count(Shorter)),
                     Shorter#wire_name.labels).
 
-compare_label(Label1, Label2) ->
-    Str1 = Label1#label.str,
-    Str2 = Label2#label.str,
-    if 
-        Str1 > Str2 -> 1;
-        Str1 < Str2 -> -1;
-        true -> 0
-    end.
-
-
-compare_labels(Labels1, Labels2) ->
-    LabelCount1 = length(Labels1),
-    LabelCount2 = length(Labels2),
-    if 
-        (LabelCount1 == 0) and (LabelCount2 > 0) -> -1;
-        (LabelCount1 > 0) and (LabelCount2 == 0) -> 1;
-        (LabelCount1 == 0) and (LabelCount2 == 0) -> 0;
-        true -> {[FirstLabel1 | Labels1Left], [FirstLabel2 | Labels2Left] } = {Labels1, Labels2},
-            case compare_label(FirstLabel1, FirstLabel2) of
-                1 -> 1;
-                -1 -> -1;
-                0 -> compare_labels(Labels1Left, Labels2Left)
-            end
-    end.
-
 compare(Name1, Name2) ->
-    compare_labels(lists:reverse(Name1#wire_name.labels), lists:reverse(Name2#wire_name.labels)).
+    labels_compare(lists:reverse(Name1#wire_name.labels), lists:reverse(Name2#wire_name.labels)).
 
 
-substract(Longer, Shorter) when is_record(Longer, wire_name) and is_record(Shorter, wire_name) ->
-    left_label_count = label_count(Longer) - label_count(Shorter),
-    #wire_name{len = len(Longer) - len(Shorter),
-               label_count = left_label_count,
-               labels = lists:sublist(Longer#wire_name.labels, left_label_count)}.
+substract(Longer, Shorter) ->
+    Left_label_count = label_count(Longer) - label_count(Shorter),
+    #wire_name{len = len(Longer) - len(Shorter) + 1,
+               label_count = Left_label_count + 1,
+               labels = lists:append(lists:sublist(Longer#wire_name.labels, Left_label_count), 
+                                    [?EMPTY_LABEL])}.
 
-concat(Prefix, Suffix) when is_record(Prefix, wire_name) and is_record(Suffix, wire_name) ->
-    #wire_name{len = len(Prefix) + len(Suffix),
-               label_count = label_count(Prefix) + label_count(Suffix),
-               labels = lists:append(Prefix#wire_name.labels, Suffix#wire_name.labels)}.
+concat(Prefix, Suffix) ->
+    #wire_name{len = len(Prefix) + len(Suffix) - 1,
+               label_count = label_count(Prefix) + label_count(Suffix) - 1,
+               labels = lists:append(lists:sublist(Prefix#wire_name.labels, label_count(Prefix) - 1), 
+                                     Suffix#wire_name.labels)}.
 
 parent(Name) ->
     case label_count(Name) of
@@ -139,15 +174,19 @@ parent(Name) ->
 %%
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
+
+from_wire_discard_left(WireData) ->
+    element(1, from_wire(WireData)).
+
 from_wire_test()->
     Com = <<3, $c, $o, $m, 0>>,
-    Name = from_wire(Com),
+    Name = from_wire_discard_left(Com),
     ?assertEqual(len(Name), 5),
     ?assertEqual(label_count(Name), 2).
 
 from_to_string_test()->
     ACom= <<1, $A, 3, $c, $O, $m, 0 >>,
-    Name = from_wire(ACom),
+    Name = from_wire_discard_left(ACom),
     Str = "a.com.",
     ?assertEqual(to_string(Name), Str),
     NewName = from_string(Str),
@@ -157,20 +196,21 @@ from_to_string_test()->
 
 is_equal_test() ->
     ACom= <<1, $A, 3, $c, $O, $m, 0 >>,
-    Name1 = from_wire(ACom),
+    Name1 = from_wire_discard_left(ACom),
     ACom2 = <<1, $A, 3, $c, $O, $m, 0 >>,
-    Name2 = from_wire(ACom2),
+    Name2 = from_wire_discard_left(ACom2),
     ?assert(is_equal(Name1, Name2)),
     ACom3 = <<1, $A, 3, $C, $O, $m, 0 >>,
-    Name3 = from_wire(ACom3),
+    Name3 = from_wire_discard_left(ACom3),
     ?assert(is_equal(Name1, Name3)),
     ACom4 = <<1, $b, 3, $C, $O, $m, 0 >>,
-    Name4 = from_wire(ACom4),
+    Name4 = from_wire_discard_left(ACom4),
     ?assertNot(is_equal(Name1, Name4)).
 
 parent_test() ->
     ACom= <<1, $A, 3, $c, $O, $m, 0 >>,
-    Name = from_wire(ACom),
+    Name = from_wire_discard_left(ACom),
+    ?assertEqual(to_wire(Name), <<1, $a, 3, $c, $o, $m, 0>>),
     Com = parent(Name),
     ?assertEqual(len(Com), 5),
     ?assertEqual(label_count(Com), 2),
@@ -179,6 +219,13 @@ parent_test() ->
     ?assertEqual(label_count(Root), 1),
     Empty = parent(Root),
     ?assertEqual(Empty, null).
+
+substract_concat_test() ->
+    COMName = from_string("COM"),
+    CNName = from_string("CN"),
+    ?assert(is_equal(concat(COMName, CNName), from_string("com.cn"))),
+    ?assert(is_equal(substract(from_string("com.cn"), CNName), COMName)).
+
 
 -endif.
 
